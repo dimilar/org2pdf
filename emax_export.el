0442772b965c3a6cf017a3a68a2069381a9aae96
Dimilar Zhu <schuleichu@gmail.com>
2014-07-10 12:50:21 +0800

    Added licence information

--- emax_export.el
+++ emax_export.el
@@ -1,3 +1,28 @@
+;;; emax_export.el --- 
+
+;; Copyright (C) 2014 Zhu Shulei
+
+;; Author: Zhu Shulei
+;; Modified by: Zhu Shulei <schuleichu@gmail.com>
+;; Created: 2014-07-10 12:48
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
 ;;;;;;;;;;;;;;;;;;;;  org export setting  ;;;;;;;;;;;;;;;;;;;;
 ;; (setq org-odt-data-dir  (concat emax-maindir "/org/odt/"))
 ;; (setq org-export-odt-schema-dir (concat emax-maindir "/org/odt/schema"))
